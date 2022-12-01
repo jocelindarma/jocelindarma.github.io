247b98ca73a8535a7e2503f08d139a5b780e2cfb
Jocelin Darma <jocelindarma13@gmail.com>
2022-12-01 05:30:54 -0800

    add snow!!

--- src/components/ImageCarousel/ImageCarousel.js
+++ src/components/ImageCarousel/ImageCarousel.js
@@ -5,25 +5,35 @@ import image4 from "../../images/memoji4.png";
 import image5 from "../../images/memoji5.png";
 import image6 from "../../images/memoji6.png";
 import image7 from "../../images/memoji7.png";
-
+import Snowfall from "react-snowfall";
+import ReactTooltip from "react-tooltip";
 import { useState, useEffect } from "react";
 
 const images = [image1, image2, image3, image4, image5, image6, image7];
 
 export const ImageCarousel = () => {
-    const [currentImage, setCurrentImage] = useState(null);
+	const [currentImage, setCurrentImage] = useState(null);
+	const [snow, setSnow] = useState(false);
 
-    useEffect(() => {
-        const intervalId = setInterval(() => {
-            setCurrentImage(images[Math.floor(Math.random() * images.length)]);
-        }, 5000)
-        
-        return () => clearInterval(intervalId);
-    }, [])
+	useEffect(() => {
+		const intervalId = setInterval(() => {
+			setCurrentImage(images[Math.floor(Math.random() * images.length)]);
+		}, 5000)
+		
+		return () => clearInterval(intervalId);
+	}, [])
 
-    return (
-        <div className="icon">
-            <img src={currentImage} alt=""/>
-        </div>
-    )
+	return (
+		<div className="icon">
+			<img src={currentImage} alt="" data-tip data-for="imageToolTip" onClick={() => setSnow(!snow)}/>
+      <ReactTooltip id="imageToolTip" place="right" effect="solid">
+        Boop my nose for a surprise!
+      </ReactTooltip>
+			{snow ? 
+				<Snowfall
+					style={{position: 'fixed'}}
+				/> : null
+			}
+		</div>
+	)
 }
