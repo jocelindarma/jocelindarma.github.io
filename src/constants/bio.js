923cba7f5ca9501a68d03255cf7b8338f0d0e458
Jocelin Darma <jocelindarma13@gmail.com>
2022-11-30 19:23:00 -0800

    update bio

--- src/constants/bio.js
+++ src/constants/bio.js
@@ -2,57 +2,77 @@ export const BIO = [
   {
     year: "July 2021 - November 2022",
     title: "Application Developer I at Starbucks Corporation",
-    description:
-      "Development of ads, covers, and social media using design methodologies such as Design Thinking",
-    details: ["Adobe Photoshop", "Adobe Illustrator", "Adobe InDesign"],
+    description:[
+      "Designed and developed microservices using React, Flask, MongoDB, and Docker \n",
+      "Wrote clear, concise and testable code and troubleshot/debugged REST APIs \n",
+      "Automated deployments via CI/CD pipelines (Drone, Ansible, Docker Swarm) \n",
+      "Created and maintained documentation supporting training, system administration, deployment, and operational processes and procedures \n",
+      "Engaged and collaborated in the Agile development environment (Scrum, Kanban) \n"
+    ],
+    skills: ["Python", "JavaScript", "TypeScript", "Flask", "Docker", "Drone", "Ansible", "MongoDB"],
   },
   {
     year: "February 2021 - July 2021",
     title: "Data Analyst at MAQ Software",
-    description:
-      "Development of ads, covers, and social media using design methodologies such as Design Thinking",
-    details: ["Adobe Photoshop", "Adobe Illustrator", "Adobe InDesign"],
+    description:[
+      "Developed Power BI custom visuals that enables people to create data visualizations for clients or internal use \n",
+      "Wrote unit tests, ensuring 100% passed test cases with > 90% code coverage \n",
+      "Utilized Power BI to pull data from various databases and created variety of dashboards and reports to support business decision making for customers \n",
+    ],
+    skills:["Microsoft Power BI", "TypeScript"],
   },
   {
     year: "December 2020",
     title: "Graduated from Seattle University",
-    description:
-      "Development of ads, covers, and social media using design methodologies such as Design Thinking",
-    details: ["Adobe Photoshop", "Adobe Illustrator", "Adobe InDesign"],
+    description:[
+      "Graduated with a BS in Computer Science",
+      "Head of Graphic Design for Indonesian Student Association of Seattle University (ISASU)",
+      "Member of Computer Science and Cybersecurity Club",
+    ],
+    skills:[],
   },
   {
     year: "June 2020 - August 2020",
     title: "Application Developer I Intern at Starbucks Corporation",
-    description:
-      "Development of ads, covers, and social media using design methodologies such as Design Thinking",
-    details: ["Adobe Photoshop", "Adobe Illustrator", "Adobe InDesign"],
+    description: [
+      "Expanded capabilities for a critical internal site, which shows the health of Starbucks Technology products and services, and improved MTTR for outages by >10%",
+      "Gathered requirements, designed, and implemented key features/enhancements to the UI and Data Integration",
+      "Contributed to the automated deployment (CI/CD) process that enhances system efficiency",
+      "Refactored the existing front-end codebase to follow new team standards",
+    ],
+    skills:["React", "Redux", "Drone", "Ansible"],
   },
   {
     year: "October 2019 - June 2020",
     title: "Software Developer at Vrbo",
-    description:
-      "Development of ads, covers, and social media using design methodologies such as Design Thinking",
-    details: ["Adobe Photoshop", "Adobe Illustrator", "Adobe InDesign"],
+    description:[
+      "Built an Android mobile application in Java and Kotlin, that allows speech-to-speech translation between 40+ languages; this will serve as a proof-of-concept for future feature integration with the Vrbo app. \n",
+      "Project involves speech-to-text and text-to-speech conversion, natural language processing, cloud integration and building the mobile application itself."
+    ],
+    skills:["Java", "Kotlin", "Android Studio", "AWS Lambda", "AWS SAM"],
   },
   {
     year: "July 2019 - September 2019",
     title: "Software Developer Intern at AiChat",
-    description:
-      "Development of ads, covers, and social media using design methodologies such as Design Thinking",
-    details: ["Adobe Photoshop", "Adobe Illustrator", "Adobe InDesign"],
+    description:[
+      "Implemented features and improved web-based content management system using React and Redux \n",
+      "Collaborated with back-end developers to integrate UI elements with server-side logic and learned data storage integration using Redis"
+    ],
+    skills:["React", "Redux"],
   },
   {
     year: "September 2018",
     title: "Student at Seattle University",
-    description:
-      "Development of ads, covers, and social media using design methodologies such as Design Thinking",
-    details: ["Adobe Photoshop", "Adobe Illustrator", "Adobe InDesign"],
+    description:[],
+    skills:[],
   },
   {
     year: "January 2017 - August 2018",
     title: "Student at North Seattle College",
-    description:
-      "Development of ads, covers, and social media using design methodologies such as Design Thinking",
-    details: ["Adobe Photoshop", "Adobe Illustrator", "Adobe InDesign"],
+    description:[
+      "Graduated with an Associates degree in Computer Science \n",
+      "Member of Phi Theta Kappa, Computer Science Club and Rocket Club",
+    ],
+    skills:[],
   },
 ];
