8140df17c0dc7ad56473bc877958f5c57c8e70c9
Jocelin Darma <jocelindarma13@gmail.com>
2022-11-30 16:17:43 -0800

    update footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,12 +1,13 @@
 import "./Footer.css";
+import {FaGithub, FaLinkedin, FaGoogle} from 'react-icons/fa'
 
 export const Footer = () => {
   return (
     <footer className="container-footer">
       <p> 
-        <a href="mailto:jocelindarma13@gmail.com" target="_top"><span className="white-text">Email</span></a>{" "}
-        <a href="https://github.com/jocelindarma" target="_blank" rel="noreferrer"><span className="white-text">Github</span></a>{" "}
-        <a href="https://www.linkedin.com/in/jocelindarma/" target="_blank" rel="noreferrer"><span className="white-text">LinkedIn</span></a>{" "}
+        <a href="mailto:jocelindarma13@gmail.com" target="_top"><span className="white-text contact"><FaGoogle/></span></a>{" "}
+        <a href="https://github.com/jocelindarma" target="_blank" rel="noreferrer"><span className="white-text contact"><FaGithub/></span></a>{" "}
+        <a href="https://www.linkedin.com/in/jocelindarma/" target="_blank" rel="noreferrer"><span className="white-text contact"><FaLinkedin/></span></a>{" "}
       </p>
       <p className="copyright"> 2022 Jocelin Darma </p>
     </footer>
