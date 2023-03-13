6cc9161daf3c9bb61dd9bf4fe5b54eeb1fa07f76
Jocelin Darma <jocelindarma13@gmail.com>
2023-03-13 15:15:46 +0700

    add tech stack

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -10,15 +10,28 @@ export const About = () => {
           About Me {" "}
         </span>
         <p className="gray-text">
-          I am a Full Stack Developer and DevOps Enthusiast. I was born and raised in
-          Jakarta, Indonesia, currently based in Seattle, Washington. My current work
-          experience includes building scalable and reliable web applications, slackbots,
+          I am a Full Stack Developer and DevOps Enthusiast. I am currently based in Jakarta, Indonesia.
+          My work experience includes building scalable and reliable web applications, slackbots,
           microservices, as well as automating and supporting to ensure CI/CD in product
           development. I am a good communicator and self-motivator, able to work as part of a team
           but also under my own initiative. I am currently open to work so feel free to contact
           me via <a href="mailto:jocelindarma13@gmail.com" target="_top">email </a>
           or <a href="https://www.linkedin.com/in/jocelindarma/" target="_blank" rel="noreferrer">LinkedIn</a>! {" "}
+          You can also check some of my work on my <a href="https://github.com/jocelindarma" target="_blank" rel="noreferrer">GitHub</a>! 
         </p>
+        <br></br>
+        <p className="gray-text">
+          Here are a few technologies I've been working with: 
+          <ul className="tech-stack">
+            <li>JavaScript (ES6+)</li>
+            <li>TypeScript</li>
+            <li>React</li>
+            <li>Node.js</li>
+            <li>Express</li>
+            <li>Python</li>
+          </ul>
+        </p>
+        <br></br>
         <span className="resume">
           <a href={resume} target="_blank" rel="noreferrer">Download My Resume</a>
         </span>
